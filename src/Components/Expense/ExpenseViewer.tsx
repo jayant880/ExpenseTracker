774bd5cf7eb7aade6265d4669090f40985df7c3e
Jayant <188503548+jayant880@users.noreply.github.com>
2025-07-17 08:30:10 +0000

    Feat: Added total

--- src/Components/Expense/ExpenseViewer.tsx
+++ src/Components/Expense/ExpenseViewer.tsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import type { ExpenseNode } from "../../types/types"
 import ExpenseRow from "./ExpenseRow"
 import { endOfMonth, endOfWeek, isWithinInterval, startOfMonth, startOfWeek } from "date-fns";
@@ -11,8 +11,9 @@ interface ViewExpenseProps {
 
 function ViewExpenses({ expenseList, updateExpense: updateExpense, deleteExpense }: ViewExpenseProps) {
     const [dateFilter, setDateFilter] = useState<"all" | "week" | "month">("all");
+    const [total, setTotal] = useState<number>(0)
 
-    const filteredExpenses = expenseList.filter(expense => {
+    const filteredExpensesList = expenseList.filter(expense => {
         const now = new Date();
         if (dateFilter === 'all') return true;
         if (dateFilter === 'week') return isWithinInterval(expense.date, {
@@ -26,7 +27,15 @@ function ViewExpenses({ expenseList, updateExpense: updateExpense, deleteExpense
         return true;
     })
 
-    if (filteredExpenses.length === 0) return (
+    useEffect(() => {
+        let expenseListTotal: number = 0;
+        filteredExpensesList.map((expenseItem) => {
+            expenseListTotal += Number(expenseItem.amount)
+        });
+        setTotal(expenseListTotal);
+    }, [filteredExpensesList])
+
+    if (filteredExpensesList.length === 0) return (
         <p>No expenses</p>
     );
 
@@ -49,10 +58,22 @@ function ViewExpenses({ expenseList, updateExpense: updateExpense, deleteExpense
                     </tr>
                 </thead>
                 <tbody>
-                    {filteredExpenses.map((expense: ExpenseNode) => (
+                    {filteredExpensesList.map((expense: ExpenseNode) => (
                         <ExpenseRow key={expense.id} expense={expense} updateExpense={updateExpense} deleteExpense={deleteExpense} />
                     ))}
                 </tbody>
+                {total !== 0 && (
+                    <tfoot>
+                        <tr>
+                            <td>Total :</td>
+                            <td>{total}</td>
+                            <td> - </td>
+                            <td> - </td>
+                            <td> - </td>
+                            <td> - </td>
+                        </tr>
+                    </tfoot>
+                )}
             </table>
         </div>
     );
